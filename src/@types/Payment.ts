bb94af3e8d5188d14ee3064601232f2925cd20f0
Alex Holanda <alex.holanda@outlook.com>
2021-09-15 14:51:30 -0300

    Criado o serviço de pagamentos

--- src/@types/Payment.ts
+++ src/@types/Payment.ts
@@ -2,9 +2,22 @@ import { AlgaNews } from "./AlgaNews";
 
 export namespace Payment {
   export type Input = AlgaNews.components["schemas"]["PaymentInput"];
-  export type PreviewInput = AlgaNews.components["schemas"]["PaymentPreviewInput"];
+  export type PreviewInput =
+    AlgaNews.components["schemas"]["PaymentPreviewInput"];
   export type Preview = AlgaNews.components["schemas"]["PaymentPreviewInput"];
   export type Detailed = AlgaNews.components["schemas"]["PaymentDetailed"];
   export type Summary = AlgaNews.components["schemas"]["PaymentSummary"];
   export type Paginated = AlgaNews.components["schemas"]["PaymentsPaginated"];
+
+  export type Query = {
+    payeeId?: number;
+    payeeEmail?: string;
+    scheduledToYearMonth?: string;
+    page?: number;
+    size?: number;
+  } & Sort;
+
+  export type Sort = {
+    sort?: [keyof Summary, "asc" | "desc"];
+  };
 }
---
+++ src/services/Payment.service.ts
@@ -0,0 +1,58 @@
+import { generateQueryString } from "..";
+import { Payment, Post } from "../@types";
+import Service from "../Service";
+
+class PaymentService extends Service {
+  static getAllPayments(search: Payment.Query) {
+    const queryString = generateQueryString(search);
+
+    return this.Http.get<Payment.Paginated>(
+      "/payments".concat(queryString)
+    ).then(this.getData);
+  }
+
+  static getExistingPayment(paymentId: number) {
+    return this.Http.get<Payment.Detailed>(`/payments/${paymentId}`).then(
+      this.getData
+    );
+  }
+
+  static insertNewPayment(payment: Payment.Input) {
+    return this.Http.post<Payment.Detailed>("/payments", payment).then(
+      this.getData
+    );
+  }
+
+  static approvePaymentsBatch(paymentIds: number[]) {
+    return this.Http.put<{}>("/payments/bulk-approvals", paymentIds).then(
+      this.getData
+    );
+  }
+
+  static getPaymentPreview(paymentPreview: Payment.PreviewInput) {
+    return this.Http.post<Payment.Preview>(
+      "/payments/previews",
+      paymentPreview
+    ).then(this.getData);
+  }
+
+  static approvePayment(paymentId: number) {
+    return this.Http.put<{}>(`/payments/${paymentId}/approval`).then(
+      this.getData
+    );
+  }
+
+  static removeExistingPayment(paymentId: number) {
+    return this.Http.delete<{}>(`/payments/${paymentId}`).then(this.getData);
+  }
+
+  static getExistingPaymentPosts(paymentId: number, sort?: Payment.Sort) {
+    const queryString = sort ? generateQueryString(sort) : "";
+
+    return this.Http.get<Post.WithEarnings>(
+      `/payments/${paymentId}/posts`.concat(queryString)
+    ).then(this.getData);
+  }
+}
+
+export default PaymentService;
