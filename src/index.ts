cfc2581b969f794492b0df4de569c11977d9d1ba
Alex Holanda <alex.holanda@outlook.com>
2021-09-15 11:22:37 -0300

    Add base services

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-export default function log() {
-  console.log("Função de log");
-}
+import * as services from "./services";
+import * as utils from "./utils";
 
-export function feature() {
-  console.log("Hi, I am a feature");
-}
+export { default as FileService } from "./services/File.service";
+export { default as MetricService } from "./services/Metric.service";
+export { default as UserService } from "./services/User.service";
+export { default as PostService } from "./services/Post.service";
+
+export { default as generateQueryString } from "./utils/generateQueryString";
+
+export default {
+  services,
+  utils,
+};
