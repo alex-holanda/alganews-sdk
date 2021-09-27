c086081639037aaca628272846cb984aeee8e24d
Alex Holanda <alex.holanda@outlook.com>
2021-09-27 20:16:15 -0300

    Exportado os erros

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as services from "./services";
 import * as utils from "./utils";
+import * as errors from "./errors";
 
 export { default as FileService } from "./services/File.service";
 export { default as MetricService } from "./services/Metric.service";
@@ -10,9 +11,19 @@ export { default as CashFlowService } from "./services/CashFlow.service";
 
 export { default as generateQueryString } from "./utils/generateQueryString";
 
+export { default as BusinessError } from "./errors/Business.error";
+export { default as ForbiddenError } from "./errors/Forbidden.error";
+export { default as GenericError } from "./errors/Generic.error";
+export { default as InvalidDatarror } from "./errors/InvalidData.error";
+export { default as InvalidParameterError } from "./errors/InvalidParameter.error";
+export { default as ResourceNotFoundError } from "./errors/ResourceNotFound.error";
+export { default as SystemError } from "./errors/System.error";
+export { default as IncomprehensibleMessage } from "./errors/IncomprehensibleMessage.error";
+
 export * from "./@types";
 
 export default {
   services,
   utils,
+  errors,
 };
